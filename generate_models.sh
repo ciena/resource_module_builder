e6c16da8819ae45193f9f6c79dbe5fe05b1eceee
Jeff Groom <jgroom@ciena.com>
2025-03-10 12:01:36 -0600

    good model list

--- generate_models.sh
+++ generate_models.sh
@@ -6,10 +6,8 @@ saos10_yangs=(
   ciena-bgp
   ciena-cfm
   ciena-dhcpv6-client
-  # ciena-flexe           # multiple
   ciena-igmp-snooping
   ciena-isis
-  # ciena-mef-access-flow # multiple
   ciena-mef-fp
   ciena-mef-logical-port
   ciena-ospf
@@ -25,53 +23,47 @@ saos10_yangs=(
   ietf-alarms
   ietf-snmp
   ietf-twamp
-  # mef-cfm               # multiple
+  ciena-mef-access-flow # multiple
+  ciena-flexe           # multiple
+  mef-cfm               # multiple
 )
 network_os=saos10
 for yang in ${saos10_yangs[@]}; do
-  echo ""
-  echo "Processing $yang.yang..."
   pyang -f ansible -n $network_os -p yangs/$network_os yangs/$network_os/$yang.yang >rmb_models/$network_os/$yang.yml
-  resource=$(yq -e .RESOURCE rmb_models/$network_os/$yang.yml)
-  mkdir -p models/$network_os/$resource
-  cp rmb_models/$network_os/$yang.yml models/$network_os/$resource/model.yml
-  echo "Finished processing $yang.yang"
-  echo ""
+  # resource=$(yq -e .RESOURCE rmb_models/$network_os/$yang.yml)
+  # mkdir -p models/$network_os/$resource
+  # cp rmb_models/$network_os/$yang.yml models/$network_os/$resource/model.yml
 done
 
 # WAVESERVERAi
 waveserverai_yangs=(
-  # ciena-waveserver-aaa
-  # ciena-waveserver-application-auto-fiber-discovery
-  # ciena-waveserver-application-otdr
-  # ciena-waveserver-chassis
-  # ciena-waveserver-configuration
-  # ciena-waveserver-interfaces
-  # ciena-waveserver-license
-  # ciena-waveserver-link-data-telemetry
-  # ciena-waveserver-lldp
-  # ciena-wavesever-logging
-  # ciena-waveserver-module
-  # ciena-waveserver-ndp
-  # ciena-waveserver-pkix
-  # ciena-waveserver-pm
-  # ciena-waveserver-pm-tca
-  # ciena-waveserver-port
-  # ciena-waveserver-protection
-  # ciena-waveserver-ptp
-  # ciena-waveserver-snmp
-  # ciena-waveserver-software
-  # ciena-waveserver-spli
+  ciena-waveserver-aaa
+  ciena-waveserver-application-auto-fiber-discovery
+  ciena-waveserver-application-otdr
+  ciena-waveserver-chassis
+  ciena-waveserver-configuration
+  ciena-waveserver-interfaces
+  ciena-waveserver-license
+  ciena-waveserver-lldp
+  ciena-wavesever-logging
+  ciena-waveserver-module
+  ciena-waveserver-ndp
+  ciena-waveserver-pkix
+  ciena-waveserver-pm
+  ciena-waveserver-pm-tca
+  ciena-waveserver-port
+  ciena-waveserver-protection
+  ciena-waveserver-ptp
+  ciena-waveserver-snmp
+  ciena-waveserver-software
+  ciena-waveserver-spli
   ciena-waveserver-system
-  # ciena-waveserver-topology
   ciena-waveserver-xcvr
 )
 network_os=waveserverai
 for yang in ${waveserverai_yangs[@]}; do
-  echo "Processing $yang.yang..."
   pyang -f ansible -n $network_os -p yangs/$network_os yangs/$network_os/$yang.yang >rmb_models/$network_os/$yang.yml
-  resource=$(yq -e .RESOURCE rmb_models/$network_os/$yang.yml)
-  mkdir -p models/$network_os/$resource
-  cp rmb_models/$network_os/$yang.yml models/$network_os/$resource/model.yml
-  echo "Finished processing $yang.yang"
+  # resource=$(yq -e .RESOURCE rmb_models/$network_os/$yang.yml)
+  # mkdir -p models/$network_os/$resource
+  # cp rmb_models/$network_os/$yang.yml models/$network_os/$resource/model.yml
 done
